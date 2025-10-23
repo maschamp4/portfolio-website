ab9fa8753ccf9827f70625e1b38d56b5f00f3f9d
Mascha <mascha@portfolio.local>
2025-10-23 17:44:30 +0200

    Remove last two images from slider gallery

--- src/data/content.js
+++ src/data/content.js
@@ -101,9 +101,7 @@ export const projects = [
         "/images/4.png",
         "/images/5.png",
         "/images/6.png",
-        "/images/7.png",
-        "/images/8.png",
-        "/images/9.png"
+        "/images/7.png"
       ]
     },
     technologies: ["AI Generation", "Art Direction", "Hybrid Workflows", "Inpainting", "Coherent product"],
